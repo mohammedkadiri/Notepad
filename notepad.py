3bc3e1169a880a24829d716634e18d546e604459
mohammedkadiri <mohammedkadiri72@gmail.com>
2019-06-20 16:12:32 +0100

    Implemented the horizontal scrollbar with word wrapping

--- notepad.py
+++ notepad.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import scrolledtext
 
 # Create a class to represent notepads
 #First create the  menu
@@ -17,7 +18,11 @@ class Notepad:
         self.menubar.add_cascade(label="View", menu=self.view_menu)
         self.menubar.add_cascade(label="Help", menu=self.help_menu)
 
+        self.text = scrolledtext.ScrolledText(master, width=10, height=2, wrap=WORD)
+        self.text.pack(expand=TRUE, fill='both')
+
         master.config(menu=self.menubar)
+
 
 
 root = Tk()
